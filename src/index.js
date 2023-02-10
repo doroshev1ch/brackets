eb1dc4b4d1d2ea28c9c9399a981332b908e1c39b
doroshev1ch <doroshevich.2002@mail.ru>
2023-02-10 18:17:19 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    // bracketsConfig.replace(/\s.,+/g,'');
+    // if (typeof bracketsConfig === 'object' && bracketsConfig.length % 2 == 0 && str.length % 2 == 0){
+    //   return true;
+    // } else {
+    //   return false
+    // }
+  
+  if (str.length % 2 == 0 ) {
+    return true;
+  } else {
+    return false;
+  }
 }
+// for (let i = 0; i < bracketsConfig.length; i++){
+        
+      // }
